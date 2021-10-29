50d200cebdd9bd9367ddc59f3402f4a18a7749e4
kamkonphrom <kamkonphrom@gmail.com>
2021-10-29 15:48:26 +0700

    login check user current

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React, { useState, useEffect } from "react";
+
 //css
 import "antd/dist/antd.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -17,7 +19,37 @@ import Login from "./component/pages/auth/Login";
 import Register from "./component/pages/auth/Register";
 import Home from "./component/pages/Home";
 
+//Redux
+import { useDispatch } from "react-redux";
+
+//functions
+import { currentUser } from "./component/functions/auth";
+
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const idTokenResult = localStorage.getItem("token");
+    //console.log("App", idTokenResult);
+    if (idTokenResult) {
+      currentUser(idTokenResult)
+        .then((res) => {
+          console.log("App", res);
+          dispatch({
+            type: "LOGGED_IN_USER",
+            payload: {
+              name: res.data.name,
+              token: idTokenResult,
+              role: res.data.role,
+              id: res.data_id,
+            },
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, [dispatch]);
   return (
     <div className="App">
       <Navbar />
--- src/component/functions/auth.js
+++ src/component/functions/auth.js
@@ -1,17 +1,27 @@
 import axios from "axios";
 
-export const registerHandler = async (user) => {
-  return await axios.post(process.env.REACT_APP_API + "/register", user, {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+export const registerHandler = async(user) => {
+    return await axios.post(process.env.REACT_APP_API + "/register", user, {
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
 };
 
-export const loginHandler = async (user) => {
-  return await axios.post(process.env.REACT_APP_API + "/login", user, {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+export const loginHandler = async(user) => {
+    return await axios.post(process.env.REACT_APP_API + "/login", user, {
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+};
+
+export const currentUser = async(authToken) => {
+    return await axios.post(
+        process.env.REACT_APP_API + "/current-user", {}, {
+            headers: {
+                authToken,
+            },
+        }
+    );
 };
